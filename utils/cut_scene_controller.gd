20a378c34d2101e945527b1b0dbef9f27a17b2e2
Johnny Cheng <johnnyc0217@gmail.com>
2026-05-09 17:35:26 +0800

    Refactor magic numbers

--- utils/cut_scene_controller.gd
+++ utils/cut_scene_controller.gd
@@ -1,10 +1,12 @@
 class_name CutSceneController
 extends Node
 
+const BAR_TWEEN_DURATION := 0.3
+const CAMERA_TWEEN_DURATION := 0.5
+
 @onready var dialogue_box: DialogueBox = $DialogueBox
 @onready var top_bar: ColorRect = $CinematicBars/TopBar
 @onready var bottom_bar: ColorRect = $CinematicBars/BottomBar
-
 
 var player: Player
 var boss: IgorBoss
@@ -13,12 +15,10 @@ var is_started := false
 
 func set_player(playerRef) -> void:
 	player = playerRef
-	print("set player")
 	try_start()
 	
 func set_boss(bossRef) -> void:
 	boss = bossRef
-	print("set boss")
 	try_start()
 
 func try_start() -> void:
@@ -26,7 +26,6 @@ func try_start() -> void:
 		return
 	if player != null and boss != null:
 		is_started = true
-		print("start")
 		start_cutscene()
 
 func start_cutscene() -> void:
@@ -45,9 +44,11 @@ func cinematic_start() -> void:
 	top_bar.visible = true
 	bottom_bar.visible = true
 	EntityManager.cutscene_started.emit()
+	var bar_height := top_bar.size.y
+	var viewport_height := get_viewport().get_visible_rect().size.y
 	var tween = create_tween().set_parallel()
-	tween.tween_property(top_bar, "position:y", 0.0, 0.3).from(-8.0)
-	tween.tween_property(bottom_bar, "position:y", 56.0, 0.3).from(64.0)
+	tween.tween_property(top_bar, "position:y", 0.0, BAR_TWEEN_DURATION).from(-bar_height)
+	tween.tween_property(bottom_bar, "position:y", viewport_height - bar_height, BAR_TWEEN_DURATION).from(viewport_height)
 	await tween.finished
 
 func boss_entry() -> void:
@@ -64,13 +65,15 @@ func dialogue() -> void:
 	dialogue_box.start_dialogue()
 	await EntityManager.dialogue_finished
 
-func finish() -> void:
+func finish() -> void:	
+	var bar_height := top_bar.size.y
+	var viewport_height := get_viewport().get_visible_rect().size.y
 	var tween = create_tween().set_parallel()
-	tween.tween_property(top_bar, "position:y", -8.0, 0.3)
-	tween.tween_property(bottom_bar, "position:y", 64.0, 0.3)
-	tween.tween_property(camera, "position:x", player.position.x, 0.5)
-	tween.tween_property(camera, "position:y", 32.0, 0.5)
+	tween.tween_property(top_bar, "position:y", -bar_height, BAR_TWEEN_DURATION)
+	tween.tween_property(bottom_bar, "position:y", viewport_height, BAR_TWEEN_DURATION)
+	tween.tween_property(camera, "position:x", player.position.x, CAMERA_TWEEN_DURATION)
 	await tween.finished
+	
 	EntityManager.cutscene_finished.emit()
 	top_bar.visible = false
 	bottom_bar.visible = false
